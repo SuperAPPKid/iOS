130f51fcf65200c2eb36bd8c58f84ec370d9e2f1
Unknown <gcgrasslight30@gmail.com>
2018-08-26 22:09:09 +0800

    complete gesture

--- Gestures/Gestures/ExpandTouchView.swift
+++ Gestures/Gestures/ExpandTouchView.swift
@@ -10,14 +10,25 @@ import UIKit
 
 class ExpandTouchView: TouchView {
     let yellowView: TouchView = {
-        let view = TouchView(name: "yellow", frame: .init(x: 250, y: 300, width: 50, height: 50))
+        let view = TouchView(name: "yellow", frame: .init(x: 200, y: 250, width: 50, height: 50))
         view.backgroundColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
         return view
     }()
     
     override init(name: String, frame: CGRect) {
         super.init(name: name, frame: frame)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(oneSingleTap(sender:)))
+//        tap.delegate = self
+        tap.cancelsTouchesInView = false
+//        tap.delaysTouchesBegan = true
+//        tap.delaysTouchesEnded = false
+        yellowView.addGestureRecognizer(tap)
         addSubview(yellowView)
+    }
+    
+    @objc func oneSingleTap(sender: UITapGestureRecognizer) {
+        let view = sender.view as! TouchView
+        print("\(view.name!) oneSingleTap")
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -25,17 +36,42 @@ class ExpandTouchView: TouchView {
     }
     
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        //擴大
+//        let expandRect = bounds.insetBy(dx: -50, dy: -50)
+//        if expandRect.contains(point) {
+//            for subview in subviews.reversed() {
+//                let newpoint = subview.convert(point, from: self)
+//                if let hitTestView = subview.hitTest(newpoint, with: event) {
+//                    return hitTestView
+//                }
+//            }
+//            return self
+//        }
+        
+        //使點擊子view正確
         let coverPoint = convert(point, to: yellowView)
         if yellowView.point(inside: coverPoint, with: event) {
-            return self
+            return yellowView
         }
+        
         return super.hitTest(point, with: event)
     }
     override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        //擴大
+        if bounds.insetBy(dx: -20, dy: -20).contains(point) {
+            return true
+        }
+        //會return yellowview
 //        coverPoint = convert(point, to: yellowView)
 //        if yellowView.point(inside: coverPoint, with: event) {
 //            return true
 //        }
         return super.point(inside: point, with: event)
+    }
+}
+
+extension ExpandTouchView: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 }
--- Gestures/Gestures/ViewController.swift
+++ Gestures/Gestures/ViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     let blueView: UIView = {
-        let view = UIView(frame: .init(x: 50, y: 50, width: 300, height: 300))
+        let view = UIView(frame: .init(x: 50, y: 50, width: 350, height: 350))
         view.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
         return view
     }()
     let greenView: ExpandTouchView = {
-        let view = ExpandTouchView(name: "green", frame: .init(x: 50, y: 50, width: 300, height: 300))
+        let view = ExpandTouchView(name: "green", frame: .init(x: 50, y: 50, width: 250, height: 250))
         view.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
         return view
     }()
@@ -34,7 +34,8 @@ class ViewController: UIViewController {
         greenView.addGestureRecognizer(oneTap1)
         pinkView.addGestureRecognizer(oneTap2)
         
-        view.addSubview(greenView)
+        view.addSubview(blueView)
+        blueView.addSubview(greenView)
         greenView.addSubview(pinkView)
     }
     
