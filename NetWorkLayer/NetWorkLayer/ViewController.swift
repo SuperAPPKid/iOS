e5f274c254fac02134162e9e32c4bd7d3c3ea9b7
Unknown <gcgrasslight30@gmail.com>
2019-01-10 18:07:06 +0800

    Start Building Network Layer

---
+++ NetWorkLayer/NetWorkLayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetWorkLayer
+//
+//  Created by Hank_Zhong on 2019/1/10.
+//  Copyright © 2019 Hank_Zhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
